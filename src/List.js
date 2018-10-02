d463dc00825249d3162762d3bb2efc4eaa4ef1e4
matt <cookem529@gmail.com>
2018-10-02 11:32:59 -0400

    list works with static state

--- src/List.js
+++ src/List.js
@@ -2,24 +2,34 @@ import React, { Component } from "react"
 
 class List extends Component {
   state = {}
+
+  renderItem=(item)=>{
+    let className=""
+    if (item.complete){
+      className="complete"
+    }
+
+    let checked=""
+    if(item.complete){
+      checked="checked"
+    }
+    return(
+      <li key={item.id} className={className}>
+        <input type="checkbox" checked={checked} />
+        {item.text} <button>edit</button>
+        <button>delete</button>
+      </li>
+    )
+  }
   render() {
+    console.log(this.props.items)
     return (
       <ul>
-        <li>
-          <input type="checkbox" />
-          todo 1 <button>edit</button>
-          <button>delete</button>
-        </li>
-        <li>
-          <input type="checkbox" />
-          todo 2 <button>edit</button>
-          <button>delete</button>
-        </li>
-        <li>
-          <input type="checkbox" />
-          todo 3 <button>edit</button>
-          <button>delete</button>
-        </li>
+        {
+          this.props.items.map(item=>{
+            return this.renderItem(item)
+          })
+        }
       </ul>
     )
   }
