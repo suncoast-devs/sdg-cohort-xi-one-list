d54cfd83412a680fb69c44900d9e9a5038ea54a2
Karsten Rogers <karstenrogers@Karstens-Air.localdomain>
2018-10-02 12:28:14 -0400

    Prettier

--- src/App.js
+++ src/App.js
@@ -1,53 +1,54 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
 import "./App.css"
 import List from "./List.js"
 import axios from "axios"
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      items:[
-       
-    ]
-    }
-  }
-  getItemsFromApi=()=>{
-    axios.get("https://one-list-api.herokuapp.com/items?access_token=illustriousvoyage").then(
-        response => {
-            console.log(response.data)
-            this.setState({items:response.data})
-        }
-    )
-  }
-  completeItem=(id)=>{
-    axios.put(`https://one-list-api.herokuapp.com/items/${id}?access_token=illustriousvoyage`,
-    {
-        "item": {
-            "complete": true
-        }
-    }).then(
-        response => {
-            this.getItemsFromApi()
-        }
-    )
-  }
-  componentDidMount=()=>{
-      this.getItemsFromApi()
-  }
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <p> My header</p>
-          <button>+ </button>
-        </header>
-        <List items={this.state.items} completeItem={this.completeItem}/>
-        <footer>my footer</footer>
-      </div>
-    )
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			items: []
+		}
+	}
+	getItemsFromApi = () => {
+		axios
+			.get(
+				"https://one-list-api.herokuapp.com/items?access_token=illustriousvoyage"
+			)
+			.then(response => {
+				console.log(response.data)
+				this.setState({ items: response.data })
+			})
+	}
+	completeItem = id => {
+		axios
+			.put(
+				`https://one-list-api.herokuapp.com/items/${id}?access_token=illustriousvoyage`,
+				{
+					item: {
+						complete: true
+					}
+				}
+			)
+			.then(response => {
+				this.getItemsFromApi()
+			})
+	}
+	componentDidMount = () => {
+		this.getItemsFromApi()
+	}
+	render() {
+		return (
+			<div className="App">
+				<header>
+					<p> My header</p>
+					<button>+ </button>
+				</header>
+				<List items={this.state.items} completeItem={this.completeItem} />
+				<footer>my footer</footer>
+			</div>
+		)
+	}
 }
 
 export default App
--- src/List.js
+++ src/List.js
@@ -1,28 +1,29 @@
 import React, { Component } from "react"
 import Item from "./Item"
 class List extends Component {
-  state = {}
+	state = {}
 
-  renderItem=(item)=>{
-    
-    return(
-      <Item id={item.id} text={item.text} complete={item.complete} completeItem={this.props.completeItem}>
-      </Item>)
-    
-  }
-  
-  render() {
-    console.log(this.props.items)
-    return (
-      <ul>
-        {
-          this.props.items.map(item=>{
-            return this.renderItem(item)
-          })
-        }
-      </ul>
-    )
-  }
+	renderItem = item => {
+		return (
+			<Item
+				id={item.id}
+				text={item.text}
+				complete={item.complete}
+				completeItem={this.props.completeItem}
+			/>
+		)
+	}
+
+	render() {
+		console.log(this.props.items)
+		return (
+			<ul>
+				{this.props.items.map(item => {
+					return this.renderItem(item)
+				})}
+			</ul>
+		)
+	}
 }
 
 export default List
